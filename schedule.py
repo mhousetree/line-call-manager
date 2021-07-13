a20c37c104655441a639921f83d67959046a01f9
mhousetree <mhousetree@gmail.com>
2021-07-13 12:09:31 +0900

    :sparkles: add reminder

--- schedule.py
+++ schedule.py
@@ -1,3 +1,4 @@
+from d import get_str_dt
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -12,6 +13,7 @@ import os
 import datetime
 import locale
 
+import d
 import r
 
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
@@ -22,11 +24,20 @@ line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 def main():
     conn = r.connect()
     date_next_call = conn.get('reserved_date')
+    dt_next_call = datetime.strptime('%Y年%m月%d日(%a) %H:%M')
 
-    dt_now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
-    text = '現在の時刻は' + dt_now.strftime('%Y年%m月%d日 %H:%M') + 'です。\n次回の通話は' +  date_next_call + 'に行われます。'
-    pushText = TextSendMessage(text)
-    line_bot_api.push_message(GROUP_ID, messages=pushText)
+    dt_now = datetime.datetime.now(
+        datetime.timezone(datetime.timedelta(hours=9))
+    )
+
+    if dt_now + datetime.timedelta(minutes=15) > dt_next_call:
+        text = date_next_call + "から通話予定です！"
+        pushText = TextSendMessage(text)
+        line_bot_api.push_message(GROUP_ID, pushText)
+    else:    
+        text = '現在の時刻は' + dt_now.strftime('%Y年%m月%d日(%a) %H:%M') + 'です。\n次回の通話は' +  date_next_call + 'に行われます。'
+        pushText = TextSendMessage(text)
+        line_bot_api.push_message(GROUP_ID, messages=pushText)
 
 if __name__ == "__main__":
     main()
